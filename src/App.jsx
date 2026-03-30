f369df8ed3f629d32c2ebefe7842ef56cdcbeffd
unknown <ishimwejennymiriotta@gmail.com>
2026-03-30 18:01:09 +0200

    how to style react components with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Header.jsx'
 import Footer from './footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
 function App(){
     return(
         <>
@@ -15,6 +16,7 @@ function App(){
             <Card/>
             <Card/>
             <Card/>
+            <Button/>
             <Footer/> 
         </>
     );
---
+++ src/Button.jsx
@@ -0,0 +1,15 @@
+function Button(){
+    const styles = {
+        backgroundColor: "rgb(82, 204, 244)",
+        color: "white",
+        padding: "1rem 2rem",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+        fontFamily: "Arial, Helvetica, sans-serif",
+    }
+    return(
+        <button style={styles}>Click Meee</button>
+    );
+}
+export default Button
