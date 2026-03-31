b56cc9d11e5964588991eb562685236c7b180dc0
unknown <ishimwejennymiriotta@gmail.com>
2026-03-31 11:48:16 +0200

    Introduction to conditional rendering in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Footer from './footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button.jsx'
+import UserGreeting from './UserGreeting.jsx'
 function App(){
     return(
         <>
             <Header/>
+            <UserGreeting isLoggedIn={true} username='Jenny' />
             <Food/>
             <Card name="Eunice" title="Chief Marketing Officer" isDesigner={true}/>
             <Card name="Jenny" title="Chief Financial Officer" isDesigner={true}/>
