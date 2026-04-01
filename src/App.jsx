bb0b82e1b8bc3a937bf5735469d6e862f622fb29
unknown <ishimwejennymiriotta@gmail.com>
2026-04-01 23:07:49 +0200

    Ways to render lists in react

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,23 @@ import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button.jsx'
 import UserGreeting from './UserGreeting.jsx'
+import List from './List.jsx'
 function App(){
+    const fruits = [
+        {id:1 , name: 'Apple', calories: 95},
+        {id:2 , name:'Orange', calories: 45 },
+        {id:3 , name:'Banana', calories: 105 },
+        {id:4 , name:'Watermelon', calories:159},
+        {id:5 , name:'coconut',calories:37}
+    ];
+    const vegetables = [
+        {id:6 , name: 'Potatoes', calories: 110},
+        {id:7 , name:'Celery', calories: 15 },
+        {id:8 , name:'Carrots', calories: 25 },
+        {id:9 , name:'Corn', calories:63},
+        {id:10 , name:'Broccoli',calories:50}
+    ];
+
     return(
         <>
             <Header/>
@@ -16,6 +32,8 @@ function App(){
             <Card name="Lina" title="Chief Executive Officer" isDesigner={false}/>
             <Card/>
             <Card/>
+            {fruits.length > 0 && <List items={fruits} category ="Fruits"/>}
+            {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
             <Button/>
             <Footer/> 
         </>
