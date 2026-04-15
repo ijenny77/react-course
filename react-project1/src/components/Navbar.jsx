de106463b1ad17dd9277aca91cf2f69426cac927
unknown <ishimwejennymiriotta@gmail.com>
2026-04-16 01:50:38 +0200

    Finalising the navbar

--- react-project1/src/components/Navbar.jsx
+++ react-project1/src/components/Navbar.jsx
@@ -5,11 +5,12 @@ import dark_logo from '../assets/image-removebg-preview (1).png'
 import light_logo from '../assets/image copy.png'
 import cross from '../assets/close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
 import arrow from '../assets/arrow_forward_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+import ThemeToggleBtn from "./ThemeToggleBtn";
 function Navbar({theme,setTheme}) {
     const [sidebarOpen,setSidebarOpen] = useState(false)
     return(
         <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70 ">
-            <img src={theme === 'dark' ? dark_logo : light_logo} alt="logo" className=" size-36 sm:w-40" />
+            <img src={theme === 'dark' ? dark_logo : light_logo} alt="logo" className=" w-32 sm:w-40" />
 
             <div className={`text-gray-700 dark:text-white sm:text-sm ${!sidebarOpen ? 'max-sm:w-0 overflow-hidden' :'max-sm:w-60 max-sm:pl-10'} max-sm:fixed top-0 bottom-0 right-0 
             max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all`}>
@@ -21,6 +22,7 @@ function Navbar({theme,setTheme}) {
                 <a onClick={() => setSidebarOpen(false)} href="#contact-us" className="sm:hover:border-b">Contact us</a>
             </div>
             <div className="flex items-center gap-2 sm:gap-4">
+                <ThemeToggleBtn theme={theme} setTheme={setTheme}/>
                 <img src={theme === 'dark' ? menu_icon_light : menu_icon_dark} onClick={()=> setSidebarOpen(true)} alt="menu icon" className="w-8 sm:hidden" />
                 <a href="#contact-us" className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all">
                     Connect <img src={arrow} alt="" />
