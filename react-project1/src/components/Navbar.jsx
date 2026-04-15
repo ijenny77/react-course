38ef288462e8a2f5fb35506d386a780dbc926158
unknown <ishimwejennymiriotta@gmail.com>
2026-04-15 23:37:57 +0200

    Initial commit of the first project in react

---
+++ react-project1/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Navbar() {
+    return(
+        <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70 "></div>
+    )
+}
+export default Navbar
