5303f6bd968a3b2b85ae8a26900af1b77137855b
ijenny77 <ishimwejennymiriotta@gmail.com>
2026-04-22 21:24:29 +0200

    Delete src directory

--- src/footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    return(
-        <footer>
-            <p className="footer">&copy;{new Date().getFullYear()} Your Website name</p>
-        </footer>
-    );
-}
-export default Footer 
